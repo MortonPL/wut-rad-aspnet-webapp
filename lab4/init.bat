023a062d6f068f2bc2bf2fe4f6f21e2f19954c43
MortonPL <MortonPL@users.noreply.github.com>
2022-01-17 15:32:49 +0100

    Install packages

---
+++ lab4/init.bat
@@ -0,0 +1,5 @@
+dotnet add package Microsoft.EntityFrameworkCore --version 5.0.13
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 5.0.13
+dotnet add package Pomelo.EntityFrameworkCore.MySql --version 5.0.3
+dotnet add package EFCore.NamingConventions --version 5.0.2
+dotnet tool install --global dotnet-ef --version 5.0.13
